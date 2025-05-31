6e8741c2c3f1c8b8b68f708b4f3550bf7e7e66cb
Liza Shulika <liza.shulika2009@gmail.com>
2025-05-31 17:55:51 +0300

    Author CRUD Implementation 5

--- author_service/settings.py
+++ author_service/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4l*aqy-h!k0a$)68da!_zw#x8)jo0r&!5aqd9gcv9z3zr)h)*="
+SECRET_KEY = \
+    "django-insecure-4l*aqy-h!k0a$)68da!_zw#x8)jo0r&!5aqd9gcv9z3zr)h)*="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,17 +90,24 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation."
+        "NAME":
+            "django.contrib.auth.password_validation."
         "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "MinimumLengthValidator",
+        "NAME":
+            "django.contrib.auth.password_validation."
+            "MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "CommonPasswordValidator",
+        "NAME":
+            "django.contrib.auth.password_validation."
+            "CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation." "NumericPasswordValidator",
+        "NAME":
+            "django.contrib.auth.password_validation."
+            "NumericPasswordValidator",
     },
 ]
 
@@ -125,13 +133,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES': [
-#         'rest_framework.renderers.JSONRenderer',
-#     ],
-#     'DEFAULT_PARSER_CLASSES': [
-#         'rest_framework.parsers.JSONParser',
-#     ],
-# }
-# AUTHORS_URL = reverse(*author:manage-list)
