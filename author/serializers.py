c6e0ee7937fbed4bf8e56fb55c2f14921eee5126
Liza Shulika <liza.shulika2009@gmail.com>
2025-05-31 17:47:03 +0300

    Author CRUD Implementation 4

--- author/serializers.py
+++ author/serializers.py
@@ -6,9 +6,11 @@ class AuthorSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(max_length=64,
-                                      allow_null=True,
-                                      required=False,)
+    pseudonym = serializers.CharField(
+        max_length=64,
+        allow_null=True,
+        required=False,
+    )
     age = serializers.IntegerField()
     retired = serializers.BooleanField()
 
@@ -20,13 +22,10 @@ class AuthorSerializer(serializers.ModelSerializer):
         return Author.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name',
-                                                 instance.first_name)
-        instance.last_name = validated_data.get('last_name',
-                                                instance.last_name)
-        instance.pseudonym = validated_data.get('pseudonym',
-                                                instance.pseudonym)
-        instance.age = validated_data.get('age', instance.age)
-        instance.retired = validated_data.get('retired', instance.retired)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.pseudonym = validated_data.get("pseudonym", instance.pseudonym)
+        instance.age = validated_data.get("age", instance.age)
+        instance.retired = validated_data.get("retired", instance.retired)
         instance.save()
         return instance
